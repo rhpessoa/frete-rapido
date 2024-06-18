01fad0684823222e66e0cafe53dcc2c029eed49b
rhpessoa <rhpessoa29@gmail.com>
2024-06-18 13:21:27 -0300

    :sparkles: feat: Alterações no componente home para consumir API

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CoinValue } from '../../../shared/interfaces/coin-value.interface'; // Importe a interface necessária
+import { Subscription } from 'rxjs';
+import { GetCoinsValueService } from '../../../shared/services/get-coins-value.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  coinsData: CoinValue[] = [];
+  private coinsDataSubscription!: Subscription;
 
+  constructor(private coinsService: GetCoinsValueService) {}
+
+  ngOnInit(): void {
+    const coins = ['CAD-BRL', 'ARS-BRL', 'GBP-BRL'];
+    this.coinsService.getCoinsValue(coins);
+    this.coinsDataSubscription = this.coinsService.coinsData$.subscribe({
+      next: (data) => {
+        this.coinsData = data;
+        this.processCoinsData();
+        console.log('Dados atualizados das moedas:', this.coinsData);
+      },
+      error: (error) => {
+        console.error('Erro ao receber dados das moedas:', error);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.coinsDataSubscription) {
+      this.coinsDataSubscription.unsubscribe();
+    }
+  }
+
+  private processCoinsData(): void {
+    this.coinsData.forEach(coin => {
+      coin.name = this.splitCoinName(coin.name)
+    })
+  }
+
+  private splitCoinName(coinName: string): string {
+    return coinName.split('/')[0].trim();
+  }
 }
