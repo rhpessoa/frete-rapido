c1b788f3c599a667fcc8dab8c598e11a6f0559de
rhpessoa <rhpessoa29@gmail.com>
2024-06-18 13:18:41 -0300

    :sparkles: feat: Criado interface de moedas

---
+++ src/app/shared/interfaces/coin-value.interface.ts
@@ -0,0 +1,13 @@
+export interface CoinValue {
+    code: string;
+    name: string;
+    high: number;
+    low: number;
+    varBid: number;
+    pctChange: number;
+    bid: number;
+    ask: number;
+    timestamp: string;
+    create_date: string;
+  }
+  
