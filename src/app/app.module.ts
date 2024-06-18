dc4c4f4bbaa418045f9234535e7686b55e2a6bf2
rhpessoa <rhpessoa29@gmail.com>
2024-06-18 13:25:08 -0300

    :wrench: chore: Configuração de Componentes, modules e componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { HomeComponent } from './core/components/home/home.component';
+import { CardComponent } from './shared/components/card/card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GetCoinsValueService } from './shared/services/get-coins-value.service';
+import { FormatBidValuePipe } from './shared/pipes/formatBidValuePipe';
+import { FormatHourPipe } from './shared/pipes/formatDatePipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CardComponent,
+    FormatBidValuePipe,
+    FormatHourPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule 
   ],
-  providers: [],
+  providers: [
+    GetCoinsValueService 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
