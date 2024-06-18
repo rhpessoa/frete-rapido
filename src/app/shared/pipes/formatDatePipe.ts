31d723f2941664e40b8c990f1e752f6927bf3fc5
rhpessoa <rhpessoa29@gmail.com>
2024-06-18 13:16:33 -0300

    :sparkles: feat: Criado pipe para formatar data para horas

---
+++ src/app/shared/pipes/formatDatePipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+
+@Pipe({ name: 'hourFormat'})
+export class FormatHourPipe implements PipeTransform {
+  transform(value: string): string {
+    const datePipe: DatePipe = new DatePipe('en-US');
+    const formattedDate = datePipe.transform(value, 'HH:mm:ss');
+    return formattedDate || '';
+  }
+}
