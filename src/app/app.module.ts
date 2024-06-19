c7c4ed9270f7d2d2372447b186f40abd075b64a0
rhpessoa <rhpessoa29@gmail.com>
2024-06-18 22:34:10 -0300

    :package: build: Upgrade do angular e dependências para a versão 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,24 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { HomeComponent } from './core/components/home/home.component';
 import { CardComponent } from './shared/components/card/card.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { GetCoinsValueService } from './shared/services/get-coins-value.service';
 import { FormatBidValuePipe } from './shared/pipes/formatBidValuePipe';
 import { FormatHourPipe } from './shared/pipes/formatDatePipe';
 import { LocalStorageService } from './shared/services/local-storage.service';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent,
-    CardComponent,
-    FormatBidValuePipe,
-    FormatHourPipe
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule 
-  ],
-  providers: [
-    GetCoinsValueService,
-    LocalStorageService,
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        CardComponent,
+        FormatBidValuePipe,
+        FormatHourPipe
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [
+        GetCoinsValueService,
+        LocalStorageService,
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
