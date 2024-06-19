4c399b98eee0b734bbc0970f26fca5ac8f7e3d5c
rhpessoa <rhpessoa29@gmail.com>
2024-06-18 21:02:04 -0300

    :sparkles: feat: Criação do serviço de armazenamento no localStorage

---
+++ src/app/shared/services/local-storage.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() { }
+
+  setItem(key: string, value: any): void {
+    const now = new Date();
+    const expiration = new Date(now.getTime() + 3 * 60000);
+    console.log(expiration)
+    const item = {
+      value: value,
+      expiration: expiration.toISOString()
+    };
+    localStorage.setItem(key, JSON.stringify(item));
+  }
+
+  getItem(key: string): any {
+    const itemStr = localStorage.getItem(key);
+    if (!itemStr) {
+      return null;
+    }
+    const item = JSON.parse(itemStr);
+    const expiration = new Date(item.expiration);
+    if (expiration <= new Date()) {
+      this.removeItem(key);
+      return null
+    }
+    return item.value;
+  }
+
+  removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  clear(): void {
+    localStorage.clear();
+  }
+
+}
