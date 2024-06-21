a39da644c56517af990d1515709c248ede5daa91
rhpessoa <rhpessoa29@gmail.com>
2024-06-21 08:24:36 -0300

    :recycle: refactor: Alteração no endpoint e lógica de requisição

--- src/app/shared/services/get-coins-value.service.ts
+++ src/app/shared/services/get-coins-value.service.ts
@@ -2,35 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { CoinValue } from '../interfaces/coin-value.interface'; 
+import { CoinValue } from '../interfaces/coin-value.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-//Tente fazer a requisição se não conseguir tente novamente. a fazer
 export class GetCoinsValueService {
-  private baseUrl = 'https://economia.awesomeapi.com.br/json/last';
-  private coinsDataSubject = new Subject<CoinValue[]>(); 
-  coinsData$: Observable<CoinValue[]> = this.coinsDataSubject.asObservable(); 
+  private baseUrl = 'https://economia.awesomeapi.com.br';
+  private coinsDataSubject = new Subject<CoinValue[]>();
+  coinsData$: Observable<CoinValue[]> = this.coinsDataSubject.asObservable();
 
   constructor(private http: HttpClient) {}
 
-  getCoinsValue(coins: string[]): void {
-    console.log("Sou as moedas", coins)
-    const coinsParam = coins.join(',');
-    const url = `${this.baseUrl}/${coinsParam}`;
+  getCoinValue(coin: string, quantity: number): Observable<CoinValue[]> {
+    const url = `${this.baseUrl}/${coin}/${quantity}`;
 
-    this.http.get<any>(url).pipe(
+    return this.http.get<any>(url).pipe(
       map(response => {
         return Object.values(response) as CoinValue[];
       })
-    ).subscribe({
-      next: (data) => {
-        this.coinsDataSubject.next(data);
-      },
-      error: (error) => {
-        console.error('Erro ao obter dados das moedas:', error);
-      }
-    });
+    );
   }
 }
