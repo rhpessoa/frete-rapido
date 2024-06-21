2c92d29af8a1b8fab43bc9e0a98687171c891063
rhpessoa <rhpessoa29@gmail.com>
2024-06-21 08:25:56 -0300

    :sparkles: feat: Criado o componente loader

---
+++ src/app/shared/loader/loader.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { LoaderService } from '../services/loader.service';
+
+
+
+@Component({
+  selector: 'app-loader',
+  templateUrl: './loader.component.html',
+  styleUrls: ['./loader.component.scss']
+})
+export class LoaderComponent implements OnInit {
+  showLoader: boolean = false;
+
+  constructor(private loaderService: LoaderService) {}
+
+  ngOnInit(): void {
+    this.loaderService.loaderState.subscribe(state => {
+      this.showLoader = state;
+    });
+  }
+}
