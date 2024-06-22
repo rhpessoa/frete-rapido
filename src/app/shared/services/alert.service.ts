c6452307c44d356d56cf3c414363ed55820b7f36
rhpessoa <rhpessoa29@gmail.com>
2024-06-22 17:04:24 -0300

    :sparkles: feat: Criação service alert

---
+++ src/app/shared/services/alert.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Subject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  private subject = new Subject<string>();
+
+  sendMessage(message: string): void {
+    this.subject.next(message);
+  }
+
+  clearMessage(): void {
+    this.subject.next('');
+  }
+
+  getMessage(): Observable<string> {
+    return this.subject.asObservable();
+  }
+}
